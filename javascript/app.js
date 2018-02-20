acc9bbfc095b94d8136d74c16691b0826398609a
Colin Williams <colin_m_williams@hotmail.com>
2018-02-20 06:04:21 -0500

    initial files

---
+++ javascript/app.js
@@ -0,0 +1,57 @@
+var questions = [{
+    "question": "Question 1",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}, {
+    "question": "Question 2",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}, {
+    "question": "Question 3",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}, {
+    "question": "Question 4",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}, {
+    "question": "Question 5",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}, {
+    "question": "Question 6",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}, {
+    "question": "Question 7",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}, {
+    "question": "Question 8",
+    "option1": "option 1",
+    "option2": "option 2",
+    "option3": "option 3",
+    "option4": "option 4",
+    "answer": "answer"
+}]
